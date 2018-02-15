1ca1349433f5a6d22751caea310765f83f6d0aaf
Ian Monroe <ian@ianmonroe.com>
2018-02-15 14:46:08 -0600

    fixed boilerplate.

--- examples_and_templates/simple_vuejs_form_example.vue
+++ examples_and_templates/simple_vuejs_form_example.vue
@@ -1,5 +1,3 @@
-// Simple example of a vue.js form, using the Axios library and bootstrap 3 markup.
-
 <style scoped>
 
 </style>
@@ -8,13 +6,17 @@
 <template>
 
     <form id="new_google_task_list" class="form-inline my-2 my-lg-0" v-on:submit.prevent="createList">
+        
         <input type="hidden" name="_token" :value="csrf">
         <input type="hidden" name="action" value="add_task_list">
+
         <div class="form-group">
             <label for="task_list_name">Create a new Task List</label>
-            <input type="text" class="form-control" id="task_list_name" placeholder="New list title" v-model="task_list_name">
+            <input type="text" class="form-control" id="task_list_name" name="task_list_name" placeholder="New list title" v-model="task_list_name">
         </div>
+
         <button type="submit" class="btn btn-default">Create</button>
+        
     </form>
 
 </template>
@@ -25,10 +27,13 @@ export default {
     mixins: [],
     data () {
       return {
+        csrf: "",  
         task_list_name: ''
       }
     },
-    mounted() {},
+    mounted() {
+        this.csrf = window.axios.defaults.headers.common['X-CSRF-TOKEN'];
+    },
     props: [
      
     ],
@@ -49,5 +54,4 @@ export default {
     }
 };
 </script>
-
 
